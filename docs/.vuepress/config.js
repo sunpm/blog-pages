213c9cfcb2ec393018a145e85f023b3ba53324c6
sunpm <15022352@qq.com>
2022-08-13 16:46:39 +0800

    清空配置文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -30,52 +30,5 @@ module.exports = {
         ],
       },
     ],
-    sidebar: [
-      {
-        title: '数据结构',
-        path: '/数据结构.md',
-      },
-      {
-        title: '数组的应用',
-        path: '/数组的应用.md',
-      },
-      {
-        title: '字符串应用',
-        path: '/字符串应用.md',
-      },
-      {
-        title: 'hash表',
-        path: '/hash表.md',
-      },
-      {
-        title: "链表",
-        children: [
-          {
-            title: "链表基础",
-            path: "/list-node/base.md",
-          },
-          {
-            title: "算法 | 链表的处理-链表的合并及删除",
-            path: "/list-node/list-instance1.md",
-          },
-        ],
-      },
-      {
-        title: '时间复杂度和空间复杂度',
-        path: '/时间复杂度和空间复杂度.md',
-      },
-      {
-        title: '递归',
-        path: '/递归/base.md',
-      },
-      {
-        title: '二分法',
-        path: '/二分法/base.md',
-      },
-      {
-        title: '广度优先搜索',
-        path: '/BFS/BFS.md',
-      },
-    ],
   },
 };
