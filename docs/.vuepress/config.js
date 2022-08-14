773f2be5aad2dfa2dd7268041cdf3e15faa3b9bd
sunpm <15022352@qq.com>
2022-08-14 10:01:36 +0800

    分离nav config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,5 @@
+const nav = require('./config/nav.config')
+
 module.exports = {
   title: 'sunpm',
   description: '成功少不了沉淀！',
@@ -20,14 +22,6 @@ module.exports = {
   },
   themeConfig: {
     subSidebar: 'auto',
-    nav: [
-      { text: '首页', link: '/' },
-      {
-        text: '了解更多',
-        items: [
-          { text: 'Github', link: 'https://github.com/sunpm' }
-        ],
-      },
-    ],
+    nav,
   },
 };
