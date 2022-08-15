a804f738edda04db4d08e27849480712aed51107
sunpm <15022352@qq.com>
2022-08-15 15:04:57 +0800

    添加百度站长

--- docs/.vitepress/config/head.config.ts
+++ docs/.vitepress/config/head.config.ts
@@ -1,7 +1,8 @@
 import {HeadConfig} from 'vitepress'
 
 const headConfig: HeadConfig[] = [
-  ['link', { rel: 'icon', href: '/images/logo.png' }]
+  ['link', { rel: 'icon', href: '/images/logo.png' }],
+  ['meta', { name: 'baidu-site-verification', content: 'code-a1SrlwvDIN' }],
 ]
 
 export default headConfig
