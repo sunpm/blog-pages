f7afed72c67a2bbe71a2b1845f9ece6b03054e2c
sunpm <15022352@qq.com>
2022-08-20 15:31:51 +0800

    添加头条站长验证

--- docs/.vitepress/config/head.config.ts
+++ docs/.vitepress/config/head.config.ts
@@ -3,7 +3,10 @@ import {HeadConfig} from 'vitepress'
 const headConfig: HeadConfig[] = [
   ['link', { rel: 'icon', href: '/images/logo.png' }],
   ['meta', { name: 'keywords', content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,css3,html5,Node,git,github,markdown' }],
+  // 百度搜索站长验证
   ['meta', { name: 'baidu-site-verification', content: 'code-a1SrlwvDIN' }],
+  // 头条搜索站长验证
+  ['meta', { name: 'bytedance-verification-code', content: 'yNTgJVlOI3MPXWXsd4jo' }],
   ['script',
     {},
     `
