c446612121f2ce30cd16bddd2e03d56928c7d7ee
sunpm <15022352@qq.com>
2024-04-19 10:47:43 +0800

    chore: test layout

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import { nextTick, provide } from 'vue'
+import { nextTick, provide, ref } from 'vue'
 
 const { isDark } = useData()
 
@@ -38,13 +38,20 @@ provide('toggle-appearance', async ({ clientX: x, clientY: y }: MouseEvent) => {
     },
   )
 })
+
+const friendList = ref([
+  'xmg',
+  'antfu',
+])
 </script>
 
 <template>
   <DefaultTheme.Layout>
     <template #doc-before>
       <div flex-center>
-        My doc-before
+        <h2 v-for="item in friendList">
+          {{ item }}
+        </h2>
       </div>
     </template>
   </DefaultTheme.Layout>
