d923f0970487d2f73ae156a674f18a94ec87a900
sunpm <15022352@qq.com>
2024-04-20 15:46:19 +0800

    feat: 修改 runLink

--- docs/.vitepress/config/url.ts
+++ docs/.vitepress/config/url.ts
@@ -1,7 +1,7 @@
 export const githubLink = 'https://github.com/sunpm/blog-pages'
 export const userGithubLink = 'https://github.com/sunpm'
 export const zhihuLink = 'https://www.zhihu.com/people/pmun'
-export const runLink = 'https://imaginative-valkyrie-e6ddc7.netlify.app'
+export const runLink = 'https://blog.fassr.com'
 
 // javascript 笔记路径
 export function getJSUrl(path: string) {
