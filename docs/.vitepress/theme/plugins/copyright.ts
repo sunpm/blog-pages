7d02c3bd5f97e54722cc9caab58f2e78bc00d830
sunpm <15022352@qq.com>
2024-04-21 20:40:40 +0800

    feat(copyright): 在非SSR环境下添加版权信息

--- docs/.vitepress/theme/plugins/copyright.ts
+++ docs/.vitepress/theme/plugins/copyright.ts
@@ -1,6 +1,9 @@
 import { META } from '../../config/app.config'
 
 function initCopyright() {
+  if (import.meta.env.SSR) {
+    return
+  }
   const copyText = () => {
     return [
       '',
@@ -10,12 +13,10 @@ function initCopyright() {
       '著作权归作者所有，商业转载请联系作者获得授权，非商业转载请注明出处。',
     ].join('\n')
   }
-
   const buildText = (content: string) => content + copyText()
   const buildHtml = (content: string) => content + copyText()
-
   document.addEventListener('copy', (event) => {
-    const content = window.getSelection()?.toString()
+    const content = window.getSelection()?.toString() || ''
     event.clipboardData?.setData('text/plain', buildText(content))
     event.clipboardData?.setData('text/html', buildHtml(content))
     event.preventDefault()
